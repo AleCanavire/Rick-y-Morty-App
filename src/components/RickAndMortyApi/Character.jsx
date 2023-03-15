5dd5895507070498b502b64491b30e2afdabe76d
AleCanavire <ale.canavire1@gmail.com>
2023-03-14 23:08:24 -0300

    Rick and Morty Api resposive
    Sección Rick and morty Api totalmente responsive y funcional.

--- src/components/RickAndMortyApi/Character.jsx
+++ src/components/RickAndMortyApi/Character.jsx
@@ -2,25 +2,23 @@ import React from 'react'
 
 function Character({ character }) {
   const status = character.status.toLowerCase();
-  const originLocation = character.origin.name === "Earth (Replacement Dimension)" ? "Earth" : character.origin.name;
-
   return (
     <div className="character-container">
       <div className={`gradient ${status}`}/>
       <div className="character-card">
-        <div className="character-image">
+        <div className={`character-image ${status}`}>
           <img src={character.image} alt={character.name} />
-          <div className={"character-status"}>
-            <span className={`status ${status}`}>{character.status}</span>
+          <div className={`character-status ${status}`}>
+            <span className="status">{character.status}</span>
           </div>
         </div>
         <div className="character-content">
           <h2 className={`character-name ${status}`}>{character.name}</h2>
           <h3 className="character-species">{character.species}</h3>
           <ul className="character-info">
-            <li className="gender"><span>Gender: </span>{character.gender}</li>
-            <li className="origin"><span>Origin: </span>{originLocation}</li>
-            <li className="location"><span>Location: </span>{originLocation}</li>
+            <li className="gender"><span>Gender</span>{character.gender}</li>
+            <li className="origin"><span>Origin</span>{character.origin.name}</li>
+            <li className="location"><span>Location</span>{character.location.name}</li>
           </ul>
         </div>  
       </div>
--- src/components/RickAndMortyApi/RickAndMortyApi.jsx
+++ src/components/RickAndMortyApi/RickAndMortyApi.jsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import Pagination from '../Pagination/Pagination';
 import Character from './Character';
 
 function RickAndMortyApi() {
-  const [characters, setCharacters] = useState([]);
+  const initialUrl = "https://rickandmortyapi.com/api/character"
 
-  const getCharacters = () => {
-    fetch("https://rickandmortyapi.com/api/character")
+  const [characters, setCharacters] = useState([]);
+  const [pages, setPages] = useState({
+    prev: null,
+    next: null
+  });
+
+  const getCharacters = (url) => {
+    fetch(url)
     .then((response) => response.json())
     .then((data) => {
       setCharacters(data.results);
+      setPages({ prev: data.info.prev, next: data.info.next })
     })
     .catch((error) => console.log(error));
   }
   useEffect(()=>{
-    getCharacters()
+    getCharacters(initialUrl)
   }, [])
   
   return (
@@ -21,6 +29,7 @@ function RickAndMortyApi() {
       <div className="logo">
         <img src="/images/rick-and-morty.png" alt="Rick and Morty Logo" />
       </div>
+      <Pagination onPrevPage={()=>getCharacters(pages.prev)} onNextPage={()=>getCharacters(pages.next)}/>
       <div className="characters">
         { characters.map((character, index)=>{
           return(
@@ -31,6 +40,7 @@ function RickAndMortyApi() {
           )})
         }  
       </div>
+      <Pagination onPrevPage={()=>getCharacters(pages.prev)} onNextPage={()=>getCharacters(pages.next)}/>
     </>
   )
 }
